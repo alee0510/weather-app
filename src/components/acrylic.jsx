5707874e396671da9311b6b29827d78aab9c3430
alee0510 <ali.muksin0510@gmail.com>
2020-06-17 19:46:03 +0700

    setup components

--- src/components/acrylic.jsx
+++ src/components/acrylic.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+const style = {
+    backgroundColor : 'black',
+    opacity : 0.9,
+    borderRadius : '15px',
+}
+
 function Acrylic () {
     return <div></div>
 }
+
+export default Acrylic
---
+++ src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Acrylic from './acrylic'
+
+class Card extends React.Component {
+    render () {
+        return (
+            <Acrylic>
+                
+            </Acrylic>
+        )
+    }
+}
+
+export default Card
