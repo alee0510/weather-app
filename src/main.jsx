b25b26700ea1600534d0afdeaaf2f1afcc597ff7
alee0510 <ali.muksin0510@gmail.com>
2020-06-18 16:07:44 +0700

    add url and api key

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
+import Axios from 'axios'
 
+// API
+const API = '&appid=fd2395620d3afcb1f6d8ac8b16d8b34a'
+const URL = 'https://api.openweathermap.org/data/2.5/weather?q='
+
+// main class
 class Main extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data : []
+        }
+    }
+
+    // async componentDidMount () {
+    //     try {
+    //         const response = await Axios.get(URL + 'Jakarta' + API)
+    //         console.log(response)
+    //     } catch(error) {
+    //         console.log(error)
+    //     }
+    // }
+
     render () {
         return (
             <div>
