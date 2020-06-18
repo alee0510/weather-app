1e89cd016fbf4fe3a87045ccfc685417125edc5a
alee0510 <ali.muksin0510@gmail.com>
2020-06-18 15:34:18 +0700

    manage assets

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+export const morning = require('./morning.jpg')
+export const day = require('./day.jpg')
+export const afternoon = require('./afternoon.jpg')
+export const night = require('./night.jpg')
