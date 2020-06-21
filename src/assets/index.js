75e0d7b5a6198877a927bd60baf8d1f91c20b813
alee0510 <ali.muksin0510@gmail.com>
2020-06-21 11:47:45 +0700

    add icons assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,12 @@
+// background
 export const morning = require('./morning.jpg')
 export const day = require('./day.jpg')
 export const afternoon = require('./afternoon.jpg')
 export const night = require('./night.jpg')
 export const noise = require('./noise.png')
+
+// icons
+export const gitHub = require('./icons/github.svg')
+export const ig = require('./icons/instagram.svg')
+export const twitter = require('./icons/twitter.svg')
+export const weatherIcon = require('./icons/weather_icon.svg')
