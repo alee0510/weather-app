6fc1b277af2e2d7e6731df715233508b3988b0e7
alee0510 <ali.muksin0510@gmail.com>
2020-07-24 21:25:48 +0700

    add raw main info and icon

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
+
 import Navbar from './components/navbar'
+
 import { morning, day, afternoon, night } from './assets'
 import { getWeather } from './actions'
+
+import { URL_ICON } from './helpers'
 
 // import style
 import './styles/main.scss'
@@ -41,16 +45,26 @@ class Main extends React.Component {
             backgroundImage : `url(${setBackground()})`,
         }
 
+        const { weather, main, name } = this.props
+
         return (
-            <div className='main-container' style={styles}>
+            <div className="main-container" style={styles}>
                 <Navbar/>
+                <div className="main-info">
+                    <img src={URL_ICON+`${weather && weather.icon}`+'@4x.png'}/>
+                    <h1>{main && main.temp} F</h1>
+                    <h1>{weather && `${weather.main}, ${weather.description}`}</h1>
+                    <h3>Location : {name}</h3>
+                </div>
             </div>
         )
     }
 }
 
 const mapStore = (state) => ({
-    weather : state.data
+    weather : state.weather,
+    main : state.main,
+    name : state.name
 })
 
 export default connect(mapStore, { getWeather })(Main)
