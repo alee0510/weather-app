30c3935ae6c7acd90a2280d45e85a92aea230fc6
alee0510 <ali.muksin0510@gmail.com>
2020-07-29 05:22:46 +0000

    Deploying to gh-pages from  @ f7a4bf2ca2bdaef27d1bcb36ecd6e8172ff0087c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.a0336c7c1f6967b6ed454569e5d494f3.js"
+  "/weather-app/precache-manifest.ede533cc1d9ab355a435293fc0882ed6.js"
 );
 
 self.addEventListener('message', (event) => {
